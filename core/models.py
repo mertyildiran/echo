0f050bdb5242c3224fd5dcd68307f4d3bb107df4
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2018-07-01 04:30:13 +0300

    Add a model named echo

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Echo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+    hearts = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created_at',)
