c3bc22ca1ac55642c513b9f485e2a6088e74dc94
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2018-07-02 01:43:29 +0300

    Add Profile model to extend User model using a One-To-One Link

--- core/models.py
+++ core/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Echo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,3 +13,24 @@ class Echo(models.Model):
 
     class Meta:
         ordering = ('created_at',)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField()
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
