21bc27a1df178a0637867c3cfd132be108919f16
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2018-07-03 07:07:17 +0300

    Add a periodic task to remove audio files older than 5 minutes

--- echo/celery.py
+++ echo/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 from django.conf import settings
+import datetime
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'echo.settings')
@@ -12,21 +13,27 @@ app = Celery('echo')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+import django
+django.setup()
+from core.models import Echo
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
-
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
-
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
-    )
+    sender.add_periodic_task(60.0, garbage_collector.s(), name='Garbage Collector')
 
 @app.task
-def test(arg):
-    print(arg)
+def garbage_collector():
+    time_ago = datetime.datetime.now() - datetime.timedelta(minutes=5)
+
+    echos = Echo.objects.filter(created_at__lte=time_ago, is_active=True).order_by('created_at')
+    for echo in echos:
+        if echo.audio and hasattr(echo.audio, 'path'):
+            print(echo.audio.path)
+            try:
+                os.remove(echo.audio.path)
+            except OSError:
+                pass
+        #echo.is_active = False
+        #echo.save(update_fields=["is_active"])
+
+    return True
