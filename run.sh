f2f0bc63522b11d0d658f729f7b2293f05a6f2b7
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2018-07-04 10:13:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup redis-server &> redis.out&
-nohup python3 manage.py runserver &> django.out&
-nohup celery -A echo worker -l info &> celery-worker.out&
-nohup celery -A echo beat -l info &> celery-beat.out&
+redis-server &
+python3 manage.py runserver &
+celery -A echo worker -l info &
+celery -A echo beat -l info
