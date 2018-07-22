fbba50cc8586b04ec536e09a0532be75da9b5069
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2018-07-22 20:19:19 +0300

    Move hearts field to ProfileAdmin and add always_active field to EchoAdmin

--- core/admin.py
+++ core/admin.py
@@ -3,11 +3,11 @@ from core.models import Echo, Profile, Notification
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    fields = ('user', 'picture', 'birth_date', 'gender', 'sexual_pref', 'bio', 'instagram', 'twitter', 'snapchat')
+    fields = ('user', 'picture', 'birth_date', 'gender', 'sexual_pref', 'bio', 'instagram', 'twitter', 'snapchat', 'hearts')
 admin.site.register(Profile, ProfileAdmin)
 
 class EchoAdmin(admin.ModelAdmin):
-    fields = ('owner', 'audio', 'location', 'hearts', 'is_active')
+    fields = ('owner', 'audio', 'location', 'is_active', 'always_active')
 admin.site.register(Echo, EchoAdmin)
 
 class NotificationAdmin(admin.ModelAdmin):
